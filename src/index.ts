03a64f1a97ed3b7aaf14ad643fdacd56f2140197
samrat-bhandari <samratbhandari01@gmail.com>
2019-09-03 20:06:26 -0600

    Genesis

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,20 @@ import { IMovies } from "./interfaces/movie.interface";
 const app = express();
 const port = 8080; // default port to listen
 
-
 // define a route handler for the default home page
 app.get("/", async (req, res) => {
+  const ourGenres: IGenres[] = await fetchGenre(
+    "https://api.themoviedb.org/3/genre/movie/list?api_key=b7ef60376b50d5774e2b4318bd6e2c45&language=en-US"
+  );
 
+  const ourMovies: IMovies[] = await fetchMovie(
+    "https://api.themoviedb.org/3/discover/movie?with_genres=18&api_key=b7ef60376b50d5774e2b4318bd6e2c45&language=en-US"
+  );
 
-    const ourGenres: IGenres[] = await fetchGenre("https://api.themoviedb.org/3/genre/movie/list?api_key=b7ef60376b50d5774e2b4318bd6e2c45&language=en-US");
-
-    const ourMovies: IMovies[] = await fetchMovie("https://api.themoviedb.org/3/discover/movie?with_genres=18&api_key=b7ef60376b50d5774e2b4318bd6e2c45&language=en-US");
-
-    res.send(ourMovies);
-
+  res.send(ourMovies);
 });
 
 // start the Express server
 app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
+  console.log(`server started at http://localhost:${port}`);
 });
--- src/utils.ts
+++ src/utils.ts
@@ -2,69 +2,56 @@ import { IGenres } from "./interfaces/genre.interface";
 import { IMovies } from "./interfaces/movie.interface";
 
 export async function fetchGenre(url: string) {
-
-    const response = await fetch(url, {
-
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-        },
-    },
-    );
-
-    if (response.ok) {
-        const json = await response.json();
-        let myGenre: IGenres[] = [];
-
-        myGenre = (json.genres as any[]).map((genre) => {
-            return { id: genre.id, name: genre.name };
-        }
-        );
-
-        return myGenre;
-
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json"
     }
+  });
 
+  if (response.ok) {
+    const json = await response.json();
+    let myGenre: IGenres[] = [];
+
+    myGenre = (json.genres as any[]).map(genre => {
+      return { id: genre.id, name: genre.name };
+    });
+
+    return myGenre;
+  }
 }
 
 export async function fetchMovie(url: string) {
-
-    const response = await fetch(url, {
-
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-        },
-    },
-    );
-
-    if (response.ok) {
-        const json = await response.json();
-        let myMovie: IMovies[] = [];
-
-        myMovie = (json.results as any[]).map((movie) => {
-
-            return {
-                popularity: movie.popularity,
-                vote_count: movie.vote_count,
-                video: movie.video,
-                poster_path: movie.poster_path,
-                id: movie.id,
-                adult: movie.adult,
-                backdrop_path: movie.backdrop_path,
-                original_language: movie.original_language,
-                original_title: movie.original_language,
-                genre_ids: movie.genre_ids,
-                title: movie.title,
-                vote_average: movie.vote_average,
-                overview: movie.overview,
-                release_date: movie.release_date
-            };
-        }
-        );
-
-        return myMovie;
-
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json"
     }
+  });
 
+  if (response.ok) {
+    const json = await response.json();
+    let myMovie: IMovies[] = [];
+
+    myMovie = (json.results as any[]).map(movie => {
+      return {
+        popularity: movie.popularity,
+        vote_count: movie.vote_count,
+        video: movie.video,
+        poster_path: movie.poster_path,
+        id: movie.id,
+        adult: movie.adult,
+        backdrop_path: movie.backdrop_path,
+        original_language: movie.original_language,
+        original_title: movie.original_language,
+        genre_ids: movie.genre_ids,
+        title: movie.title,
+        vote_average: movie.vote_average,
+        overview: movie.overview,
+        release_date: movie.release_date
+      };
+    });
+
+    return myMovie;
+  }
 }
