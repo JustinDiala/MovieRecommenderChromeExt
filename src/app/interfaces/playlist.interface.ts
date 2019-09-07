aff3c385a4ff459d19dcd31f2e7f50896ca97b38
JustinDiala <40872918+JustinDiala@users.noreply.github.com>
2019-09-07 13:51:39 -0600

    added new things in the interface

--- src/app/interfaces/playlist.interface.ts
+++ src/app/interfaces/playlist.interface.ts
@@ -56,3 +56,87 @@ export interface IFetchedMyPlaylistTypings {
   status: Number;
   statusText: string;
 }
+
+//////////////
+
+export interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
+
+export interface Default {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Medium {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface High {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Standard {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Maxres {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Thumbnails {
+  default: Default;
+  medium: Medium;
+  high: High;
+  standard: Standard;
+  maxres: Maxres;
+}
+
+export interface ResourceId {
+  kind: string;
+  videoId: string;
+}
+
+export interface Snippet {
+  publishedAt: Date;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: Thumbnails;
+  channelTitle: string;
+  playlistId: string;
+  position: number;
+  resourceId: ResourceId;
+}
+
+export interface ContentDetails {
+  videoId: string;
+  videoPublishedAt: Date;
+}
+
+export interface Item {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: Snippet;
+  contentDetails: ContentDetails;
+}
+
+export interface RootObject {
+  kind: string;
+  etag: string;
+  nextPageToken: string;
+  pageInfo: PageInfo;
+  items: Item[];
+}
+
